8fde3690387c5fdca1eeb5aea220d7cafe55628f
sumdul <sumdul22@naver.com>
2020-07-07 19:15:53 +0900

    20200707

--- BAEKJOON/1110.py
+++ BAEKJOON/1110.py
@@ -3,17 +3,76 @@
 # 8 + 4 = 12
 # 4 + 2 = 6
 # 2 + 6
+# # 4
 
 # 55 10
 # 5 0 5
 # 0 5 5
 # 5 5
+# # 3
 import sys
-N = list(input())
-print(N)
-
+# N = input()
+# print(N)
 # s =list(N)
 # print(int(s[0]))
 # # while True:
+N = "1"
+N = N + "0"
+A = int(N[0])
+B = int(N[1])
+a = A
+b = B
+count = 0
+nsum = 0
+while True:
+    if b >= 10:
+        b -= 10
+        print("b뺀다")
+    elif A == a and B == b:
+        print("yo")
+        if count > 0:
+            print(count)
+            break
+        else:
+            nsum = a + b
+            a = b
+            b = nsum
+            print("po", count, nsum, a, b)
+            count += 1
+    else:
+        nsum = a + b
+        a = b
+        b = nsum
+        print("po", count, nsum, a, b)
+        count += 1
+
+# C = A + B
+# to += 1
 # print(int(N[0]) + int(N[1]))
 
+
+N = input()
+N = "1"
+N = N + "0"
+A = int(N[0])
+B = int(N[1])
+a = A
+b = B
+count = 0
+nsum = 0
+while True:
+    if b >= 10:
+        b -= 10
+    elif A == a and B == b:
+        if count > 0:
+            break
+        else:
+            nsum = a + b
+            a = b
+            b = nsum
+            count += 1
+    else:
+        nsum = a + b
+        a = b
+        b = nsum
+        count += 1
