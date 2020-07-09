c0413924f8467a028abec4eddd7aae0c3d568745
sumdul <59238313+sumdul@users.noreply.github.com>
2020-07-09 22:02:22 +0900

    20200709

--- PythonWorkspace/20. 튜플.py
+++ PythonWorkspace/20. 튜플.py
@@ -1,1 +1,15 @@
 # 튜플
+menu = ("돈까스", "치즈까스")
+print(menu[0])
+print(menu[1])
+
+# menu.add("생선까스")
+# AttributeError: 'tuple' object has no attribute 'add'
+
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name, age, hobby)
+
+(name, age, hobby) = ("김종국", 20, "코딩")
+print(name, age, hobby)
